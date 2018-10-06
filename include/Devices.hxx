4175ac830ea3f52c299b371b14bb9b5d989a1d8d
Iftikhar Khan <iakhan4425@gmail.com>
2018-10-06 19:48:55 -0400

    Added Devices base class for inheriting all the device queues

---
+++ include/Devices.hxx
@@ -0,0 +1,32 @@
+#include <queue>
+#include <pair>
+
+#include "Meta.hxx"
+#include "PCB.hxx"
+
+using std::queue;
+using std::pair;
+
+class Device {
+private:
+  queue< pair<PCB*, metaInfo> > deviceQueue;
+protected:
+  virtual void addProcess(pair<PCB*, metaInfo>);
+public:
+  PCB* getProcess();
+};
+
+class Printer: public Device {
+public:
+  void intake(PCB*);
+};
+
+class Flash: public Device {
+public:
+  void intake(PCB*);
+};
+
+class Disk: public Device {
+public:
+  void intake(PCB*);
+}
