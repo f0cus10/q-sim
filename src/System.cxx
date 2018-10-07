ffba60fc790e77a61ab41f7cfa8732bf3ebb0fd9
Iftikhar Khan <iakhan4425@gmail.com>
2018-10-06 21:03:00 -0400

    Continued development for device allocation during sysgen

---
+++ src/System.cxx
@@ -0,0 +1,34 @@
+#include "System.hxx"
+#include "Devices.hxx"
+
+System::System(int p, int d, int f) {
+  //Generate the number of devices specified
+  for(unsigned int i = 0; i < p; ++p) {
+    printers.push_back(Printer(i));
+  }
+
+  for (unsigned int i = 0; i < d; ++d) {
+    hdd.push_back(Disk(i));
+  }
+
+  for(unsigned int i = 0; i < f; ++f) {
+    flashd.push_back(Flash(i));
+  }
+}
+
+void System::readyProcess(PCB* process){
+  read_q.push(process);
+}
+
+void System::terminateProcess(){
+  //Kill the process in the cpu
+  delete currentProcess;
+  //feed the next process
+  if (ready_q.size() > 0) {
+    currentProcess = ready_q.pop();
+  }
+  else {
+    currentProcess = nullptr;
+  }
+  return;
+}
