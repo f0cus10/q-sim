38e915c6739c9e11e30bede1427fa2be3df4882c
Iftikhar Khan <iakhan4425@gmail.com>
2018-11-24 16:49:51 -0500

    Implemented all the essential queue functions

--- src/Disk_Q.cpp
+++ src/Disk_Q.cpp
@@ -16,3 +16,30 @@ bool DiskQ::descending_empty() const {
   return descending.empty();
 }
 
+unsigned int DiskQ::size(){
+  return ascending.size() + descending.size();
+}
+
+disk_process DiskQ::ascending_top() const {
+  return ascending.top();
+}
+
+disk_process DiskQ::descending_top() const {
+  return descending.top();
+}
+
+void DiskQ::push_ascending(disk_process process){
+  ascending.push(move(process));
+}
+
+void DiskQ::push_descending(disk_process process){
+  descending.push(move(process));
+}
+void DiskQ::pop_ascending(){
+  ascending.pop();
+}
+
+void DiskQ::pop_descending(){
+  descending.pop();
+}
+
